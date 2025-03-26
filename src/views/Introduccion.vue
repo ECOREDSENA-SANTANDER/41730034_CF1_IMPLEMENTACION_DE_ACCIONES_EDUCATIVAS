9ad10469d0ec298d3de4aaf00db12b0ace7e894e
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-03-26 17:08:56 -0500

    Avance Maquetación 26.03

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,11 +9,21 @@
           i.fas.fa-info
       h1 Introducción
     
-    .row.justify-content-center.align-items-center           
-      .col-lg-12
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/img_intro.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 La formación en entornos saludables en Colombia es clave para fortalecer competencias pedagógicas que promuevan el bienestar y la calidad de vida a nivel local, enmarcada en el plan decenal de salud pública (PDSP) 2012-2021 y la estrategia de entornos saludables (EES), esta iniciativa impulsa acciones educativas enfocadas en sostenibilidad ambiental, salud pública y equidad en el acceso a servicios sanitarios, basada en la carta de Ottawa de 1986, fomenta la creación de espacios saludables, abordando los desafíos de implementación a través de estrategias metodológicas innovadoras y modelos pedagógicos efectivos.
+
+    .row.justify-content-center.align-items-center.mt-5         
+      .col-lg-10.col-sm-12.col-md-12
         figure
           .video
             iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+
+
 
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,20 @@
       .titulo-principal.color-acento-contenido
         .titulo-principal__numero
           span 1
-        h1 Titulo de primer nivel
-        
+        h1 Desarrollo de la estrategia de entornos saludables (EES)
+
+      .row.align-items-end.justify-content-center.mb-5
+        .col-sm-12.col-md-12.col-lg-5
+          .row.bgmorado.borde-redondo
+            p.p-4.pb-2 La implementación de la estrategia de entornos saludables (EES) requiere la articulación de diversos sectores, incluyendo el político, económico, cultural, ambiental y social, para garantizar el bienestar integral de la población, esto implica asegurar el acceso a empleo, educación, recreación y servicios de salud, considerando estos aspectos como factores esenciales para la construcción de entornos saludables y sostenibles, su desarrollo se enmarca en un enfoque intersectorial que promueve la equidad, la inclusión y la mejora de la calidad de vida en todas las etapas del ciclo vital.
+        .col-6.col-md-6.col-lg-3
+          img.mt-2(src='@/assets/curso/temas/t1/img1.png', alt='')
+        .col-sm-12.col-md-12.col-lg-4
+          .row.align-items-center
+            .col-auto
+              img(src='@/assets/curso/temas/t1/ico1.png', alt='').d-none.d-lg-block
+          .row.mt-3
+            p La organización mundial de la salud (OMS) y la organización panamericana de la salud (OPS) definen los elementos conceptuales esenciales para la promoción de entornos saludables, los cuales abarcan diversos ámbitos de interacción social y territorial.
 
   </template>
 
