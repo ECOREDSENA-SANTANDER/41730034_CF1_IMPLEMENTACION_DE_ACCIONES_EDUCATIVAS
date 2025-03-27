7683a292b829bd4b20df725d3697c474962bd6be
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-03-27 16:17:02 -0500

    Avance Maquetación 27.03.2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,250 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.borde-recto-abajo
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Fases de implementación de entornos saludables
+
+    .row.justify-content-center.align-items-center.bgmorado.mb-3(data-aos="fade-left")
+      .col-auto
+        img(src='@/assets/curso/temas/t2/ico1.png', alt='')
+      .col
+        p.py-3.mb-0 La creación de entornos saludables es un proceso estructurado que busca mejorar las condiciones de vida de las comunidades a través de estrategias sostenibles en salud, educación, vivienda y tecnología, para lograrlo, es fundamental seguir una serie de fases de implementación, que permiten una planificación efectiva, la ejecución de intervenciones adecuadas y la evaluación del impacto de las acciones realizadas.
+    
+    p.mb-4 Estas fases garantizan que las soluciones adoptadas sean integrales, participativas y adaptadas a las necesidades locales, promoviendo el bienestar individual y colectivo, a continuación, se describen: 
+
+    .row.justify-content-center.align-items-center(data-aos="fade")
+      .col-lg-12
+        TabsA.color-acento-botones
+          .tarjeta.color-primario.p-4(titulo="Sensibilización")
+            p.mb-3 Logra la integración de autoridades locales, comunidad y actores clave del proceso.
+            p.mb-2 #[b Componentes:]
+            ul.lista-ul--color.fa-al
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Conformación del equipo técnico de salud.        
+              li.mb-0             
+                i.fas.fa-check-circle
+                span.mb-0 Selección y actualización de la información local.       
+              li.mb-0            
+                i.fas.fa-check-circle
+                span.mb-0 Sensibilización al alcalde y consejo municipal.     
+              li.mb-0             
+                i.fas.fa-check-circle
+                span.mb-0 Integración de comunidad y actores sociales.   
+            p.mb-2 #[b Actores:]
+            ul.lista-ul--color.fa-al
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Conformación del equipo técnico de salud.        
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Municipalidad (alcaldes y gobernadores).
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Comunidad (grupos comunitarios y sociales).
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Sector público y privado (gobiernos regionales, ministerios, secretarías, universidades, iglesias, ONG).
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Sector salud (equipo técnico).
+            p.mb-2 #[b Funciones:]
+            ul.lista-ul--color.fa-al
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Promover la participación de autoridades locales.   
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Gestionar la formulación de políticas públicas saludables.
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Concertar y sensibilizar a la comunidad para recoger información útil en la definición de objetivos.
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Articular funciones en pro de las metas a lograr.
+              li.mb-0 
+                i.fas.fa-check-circle.bgvineta
+                span.mb-0 Facilitar la estrategia a través de acciones enfocadas en el empoderamiento de actores y líderes comunitarios.
+          .tarjeta.color-primario.p-4(titulo="Organización")
+            p Define y asigna roles y responsabilidades para cada integrante del equipo.
+            p.mb-2 #[b Componentes:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Conformación y establecimiento del comité multisectorial.        
+              li.mb-0             
+                i.fas.fa-check-circle  
+                span.mb-0 Desarrollo de competencias del comité multisectorial.       
+            p.mb-2 #[b Actores:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Equipo técnico.        
+            p.mb-2 #[b Funciones:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Fomentar la organización del comité multisectorial.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Promover su fortalecimiento a través de actividades de capacitación y concertación.  
+          .tarjeta.color-primario.p-4(titulo="Planificación participativa")
+            p Ajustar los planes de desarrollo en correspondencia con la estrategia para garantizar la gestión hacia un entorno o municipio saludable.
+            p.mb-2 #[b Componentes:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Reorientación del plan de desarrollo local.        
+              li.mb-0             
+                i.fas.fa-check-circle  
+                span.mb-0 Reconocimiento de la nueva orientación del plan de desarrollo.       
+            p.mb-2 #[b Actores:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Comité multisectorial.        
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Comunidad.        
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Municipalidad.        
+            p.mb-2 #[b Funciones:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Analizar el plan de desarrollo identificando ejes, metas y acciones.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Fomentar la participación activa de la comunidad.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Impulsar el reconocimiento e integración de nuevos actores clave en el proceso.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Promover el trabajo intersectorial y la conformación de redes de apoyo.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Integrarse al proceso y facilitar la reorientación del plan de desarrollo.            
+          .tarjeta.color-primario.p-4(titulo="Seguimiento, monitoreo y evaluación")
+            p Conocer el estado de los procesos para corregir, reformular, modificar o fortalecer las propuestas y acciones desarrolladas.
+            p.mb-2 #[b Componentes:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Seguimiento y monitores.        
+              li.mb-0             
+                i.fas.fa-check-circle  
+                span.mb-0 Evaluación participativa.       
+            p.mb-2 #[b Actores:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Equipo técnico y comité multisectorial.        
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Todos los actores involucrados.        
+            p.mb-2 #[b Funciones:]  
+            ul.lista-ul--color.fa-al  
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Evaluar la primera fase y articularla al plan de trabajo del comité multisectorial.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Diseñar programas alineados con políticas y recursos para entornos saludables.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Establecer metodologías y estrategias para la implementación del proceso.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Preparar indicadores, recolección de información y toma de decisiones.   
+              li.mb-0   
+                i.fas.fa-check-circle.bgvineta  
+                span.mb-0 Intervenir según roles y funciones en cada fase del proceso.  
+
+    Separador  
+    #t_2_1.titulo-segundo.color-acento-contenido(data-aos="zoom-in-left")
+      h2 2.1 Estrategia de escuela saludable
+
+    .row.align-items-center.justify-content-center.mb-4(data-aos="fade-right")
+      .col-sm-12.col-md-12.col-lg-5
+        .row.bgmorado.borde-redondo
+          p.p-4.mt-2 La organización mundial de la salud (OMS) y la organización panamericana de la salud (OPS) definen la escuela saludable como un entorno educativo donde la comunidad escolar no solo adquiere conocimientos, sino que también fomenta el desarrollo del pensamiento crítico, la afectividad y los hábitos saludables, en este espacio, se construye y reproduce la cultura, se fortalecen los valores y se promueven mejores formas de convivencia y bienestar integral.
+      .col-6.col-md-6.col-lg-3
+        img(src='@/assets/curso/temas/t2/img1.png', alt='')
+      .col-sm-12.col-md-12.col-lg-4
+        p.mb-0 La estrategia de escuela saludable (EES) tiene como objetivo principal impulsar el desarrollo humano de los escolares mediante acciones integrales de promoción de la salud y prevención de enfermedades, tomando la escuela como un eje central de bienestar, esta estrategia busca generar entornos sostenibles para niños, niñas y jóvenes, promoviendo el desarrollo de habilidades y competencias que les permitan cuidar su salud y la de su comunidad, fortaleciendo la equidad, la inclusión y la participación activa en la construcción de un futuro más saludable.
+
+    p.mb-0 La organización panamericana de la salud (OPS) establece una serie de objetivos clave para fortalecer la atención primaria ambiental, alineados con los principios del desarrollo sostenible y la gestión ambiental eficiente. Estos objetivos incluyen:
+
+  .container.tarjeta.tarjeta--blanca.fondotema2.p-md-5.mb-5.borde-recto
+    .row.align-items-center.justify-content-center.mb-4
+      .col-sm-12.col-md-12.col-lg-10
+        PasosA.color-acento-contenido.mb-0(tipo="l")
+          .row.align-items-center.justify-content-center
+            .col-lg
+              p.mb-0 Promover la creación de comunidades y municipios saludables, garantizando entornos sostenibles y resilientes.
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-1.png', alt='')
+          .row.align-items-center.justify-content-center
+            .col-auto
+              figure(data-aos="zoom-in")
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-2.png', alt='')
+            .col-lg
+              p Fortalecer la capacidad de gestión ambiental de los gobiernos locales, proporcionando herramientas y recursos para la toma de decisiones basadas en evidencia.
+          .row.align-items-center.justify-content-center  
+            .col-lg  
+              p Implementar un sistema integral de gestión ambiental local, que involucre efectivamente a todos los sectores sociales, institucionales y comunitarios.  
+            .col-auto  
+              figure(data-aos="zoom-in")  
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-3.png', alt='')  
+          .row.align-items-center.justify-content-center  
+            .col-auto  
+              figure(data-aos="zoom-in")  
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-4.png', alt='')  
+            .col-lg  
+              p Empoderar a las comunidades para alcanzar la sostenibilidad local, fomentando su participación en la planificación y ejecución de políticas ambientales.  
+          .row.align-items-center.justify-content-center  
+            .col-lg  
+              p Formar y capacitar líderes ambientales, que promuevan la concienciación ecológica y la intervención en estrategias de protección y conservación del entorno.  
+            .col-auto  
+              figure(data-aos="zoom-in")  
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-5.png', alt='')  
+          .row.align-items-center.justify-content-center  
+            .col-auto  
+              figure(data-aos="zoom-in")  
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-6.png', alt='')  
+            .col-lg  
+              p Facilitar la cooperación entre el sector público, la sociedad civil y el sector privado, con el fin de establecer compromisos y estrategias alineadas con los objetivos de desarrollo sostenible (ODS).  
+          .row.align-items-center.justify-content-center  
+            .col-lg  
+              p Fomentar el apoyo estatal a iniciativas ambientales locales, priorizando intervenciones públicas en la protección de la salud humana y la conservación del medio ambiente.  
+            .col-auto  
+              figure(data-aos="zoom-in")  
+                img.d-none.d-lg-block(src='@/assets/curso/temas/t2/ps1-7.png', alt='')  
+
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.borde-recto-arriba
+
+      .row.justify-content-center.align-items-center.bgmorado.mb-5(data-aos="fade-left")
+        .col-auto
+          img(src='@/assets/curso/temas/t2/ico2.png', alt='')
+        .col
+          p.py-3.mb-0 Según la organización panamericana de la salud (OPS) y la Organización mundial de la salud (OMS), el trabajo en los entornos constituye una oportunidad clave para la acción intersectorial, dado que se trata de un proceso complejo que demanda movilización social y una gestión política efectiva, es fundamental contar con el compromiso y la participación activa de diversos actores, estos incluyen organismos gubernamentales, instituciones académicas, el sector privado, organizaciones de la sociedad civil y la comunidad en general, cuya cooperación es esencial para la implementación y sostenibilidad de las estrategias, a continuación se describen los actores.
+
+      .row.align-items-center.mb-5.subt(data-aos="fade-right")
+        .col-auto.ico
+          img(src='@/assets/curso/temas/subt.svg', alt='')
+        .col
+          h3.mb-0.mx-3 Actores en la ejecución de la estrategia de entornos saludables (EES)
+
+      p.mb-4 La ejecución de la estrategia de entornos saludables (EES) requiere la participación de diversos actores a nivel internacional, nacional, departamental, local y comunitario, a continuación, se presentan los principales actores involucrados en cada nivel:
+
+
+
 
 </template>
 
