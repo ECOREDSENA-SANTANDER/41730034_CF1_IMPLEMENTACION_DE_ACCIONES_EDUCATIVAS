a5b1e69721c97b9742ff6c286603052f8408e817
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-03-27 11:11:01 -0500

    Avance Maquetación 27.03

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,7 +7,7 @@
           span 1
         h1 Desarrollo de la estrategia de entornos saludables (EES)
 
-      .row.align-items-end.justify-content-center.mb-5
+      .row.align-items-end.justify-content-center.mb-5(data-aos="fade-down")
         .col-sm-12.col-md-12.col-lg-5
           .row.bgmorado.borde-redondo
             p.p-4.pb-2 La implementación de la estrategia de entornos saludables (EES) requiere la articulación de diversos sectores, incluyendo el político, económico, cultural, ambiental y social, para garantizar el bienestar integral de la población, esto implica asegurar el acceso a empleo, educación, recreación y servicios de salud, considerando estos aspectos como factores esenciales para la construcción de entornos saludables y sostenibles, su desarrollo se enmarca en un enfoque intersectorial que promueve la equidad, la inclusión y la mejora de la calidad de vida en todas las etapas del ciclo vital.
@@ -19,6 +19,105 @@
               img(src='@/assets/curso/temas/t1/ico1.png', alt='').d-none.d-lg-block
           .row.mt-3
             p La organización mundial de la salud (OMS) y la organización panamericana de la salud (OPS) definen los elementos conceptuales esenciales para la promoción de entornos saludables, los cuales abarcan diversos ámbitos de interacción social y territorial.
+
+      .row.justify-content-center.align-items-center.mb-3
+        .col-6.col-md-6.col-lg-3
+          img.mb-2(src='@/assets/curso/temas/t1/img2.png', alt='')
+        .col-sm-12.col-md-12.col-lg-9
+          AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+            .row.col-xl-11.m-auto(titulo="Entorno")
+              .row.justify-content-center.align-items-center(data-aos="fade-right")
+                .col-lg-12
+                  p.mb-0 Hace referencia al espacio donde las personas interactúan activamente con su medio, incluyendo el espacio físico, biológico, ecosistema y comunidad, influyendo directamente en su bienestar y calidad de vida.
+            .row.col-xl-11.m-auto(titulo="Entornos Saludables")
+              .row.justify-content-center.align-items-center(data-aos="fade-right")
+                .col-lg-12
+                  p.mb-0 Son aquellos que favorecen la salud y brindan protección frente a factores de riesgo, permitiendo a las personas fortalecer sus capacidades, desarrollar autonomía y mejorar su calidad de vida.
+            .row.col-xl-11.m-auto(titulo="Estrategias de Entornos Saludables")
+              .row.justify-content-center.align-items-center(data-aos="fade-right")
+                .col-lg-12
+                  p.mb-0 Busca fortalecer los territorios mediante procesos sociales que reduzcan los riesgos y potencien factores protectores en salud, especialmente en comunidades vulnerables, además, fomenta oportunidades de desarrollo sostenible y promueve condiciones seguras para la población.
+            .row.col-xl-11.m-auto(titulo="Vivienda Saludable")
+              .row.justify-content-center.align-items-center(data-aos="fade-right")
+                .col-lg-12
+                  p.mb-0 Se refiere a un espacio de residencia que, a través de condiciones óptimas, favorece la restauración, protección y promoción de la salud, además de estimular la creatividad, el aprendizaje y el bienestar de sus habitantes.
+            .row.col-xl-11.m-auto(titulo="Escuela Saludable")
+              .row.justify-content-center.align-items-center(data-aos="fade-right")
+                .col-lg-12
+                  p.mb-0 Espacio educativo que fomenta el desarrollo integral de la comunidad, promoviendo el aprendizaje, la convivencia armónica, la construcción del conocimiento y el desarrollo de hábitos saludables, impactando positivamente en la calidad de vida de estudiantes y docentes.
+
+      p.mb-5 Estos elementos conceptuales constituyen la base para la formulación de políticas públicas y estrategias de intervención que buscan garantizar ambientes seguros, sostenibles e inclusivos, promoviendo el bienestar físico, mental y social de la población.
+
+      .row.align-items-center.mb-5.subt(data-aos="fade-right")
+        .col-auto.ico
+          img(src='@/assets/curso/temas/subt.svg', alt='')
+        .col
+          h3.mb-0.mx-3 Determinantes de la salud
+
+      .row.align-items-center.justify-content-center.mb-3(data-aos="fade-down")
+        .col-sm-10.col-md-10.col-lg-5
+          img(src='@/assets/curso/temas/t1/img3.png', alt='')
+        .col-sm-12.col-md-12.col-lg-7
+          p.mb-3 La organización mundial y panamericana de la salud (OMS-OPS) han desarrollado un modelo actualizado que reconoce múltiples factores que influyen en la salud de la población, entre ellos, se destacan el ingreso, la posición social, las redes de apoyo, la educación, el empleo, las condiciones de trabajo y el acceso a los servicios de salud, entre otros, estos determinantes juegan un papel clave en la promoción de entornos saludables, ya que afectan directamente el bienestar físico, mental y social de las personas, el ingreso y la posición social influyen en el acceso a recursos esenciales, mientras que las redes de apoyo fortalecen la resiliencia frente a adversidades, la educación es un factor determinante para la toma de decisiones saludables y el acceso a mejores oportunidades laborales.
+          .row.justify-content-center.bgmorado.borde-redondo
+            .col-auto
+              .row.align-items-center
+                img.mt-2(src='@/assets/curso/temas/t1/ico2.png', alt='').d-none.d-lg-block
+            .col
+              p.p-1.mb-0 El empleo y las condiciones de trabajo impactan la estabilidad económica y el bienestar personal, mientras que los entornos físicos deben garantizar espacios seguros y saludables, las características biológicas y los hábitos personales de salud influyen en la prevención de enfermedades y la calidad de vida, el desarrollo infantil en las primeras etapas es crucial para el bienestar a largo plazo, y el acceso a servicios de salud integrales reduce desigualdades y mejora la calidad de vida de la población.
+
+      p.mb-5 Este modelo enfatiza la necesidad de un enfoque intersectorial e inclusivo para abordar los determinantes de la salud, garantizando condiciones equitativas que permitan el desarrollo de comunidades más saludables y sostenibles.
+
+      .row.align-items-center.mb-5.subt(data-aos="fade-right")
+        .col-auto.ico
+          img(src='@/assets/curso/temas/subt.svg', alt='')
+        .col
+          h3.mb-0.mx-3 Marco normativo
+
+      .row.justify-content-center.align-items-center.mb-4(data-aos="fade-down")
+        .col-lg-9.col-sm-12.col-md-12
+          .row.mb-2
+            .col-12.col-md-8.col-lg-5.p-0.bgamarillo
+              img(src='@/assets/curso/temas/t1/img6.png', alt='')
+            .col-12.col-md-12.col-lg-7.bgmorado
+              p.p-4.mt-2 Los antecedentes de las estrategias de entornos saludables (EES) se fundamentan en declaraciones y acuerdos internacionales promovidos por organismos multilaterales como la UNESCO, la organización mundial de la salud (OMS) y la organización panamericana de la salud (OPS), estas estrategias buscan fortalecer la promoción de la salud con un enfoque basado en la equidad, el desarrollo de políticas públicas saludables, la construcción de municipios saludables y sostenibles, y la participación social activa.
+          p.mt-4 A nivel normativo, la EES se alinea con marcos internacionales y nacionales que promueven el bienestar integral de la población a través de acciones intersectoriales, la prevención de enfermedades y la generación de entornos que garanticen una mejor calidad de vida, además, estas estrategias han evolucionado para responder a los desafíos actuales en salud pública, integrando principios de sostenibilidad, resiliencia comunitaria y gobernanza participativa.
+        .col-lg-3.col-sm-6.col-md-6
+          img(src='@/assets/curso/temas/t1/img4.png', alt='').d-none.d-lg-block
+
+      .row.justify-content-center.mt-4.mb-5(data-aos="fade-down") 
+        .col-lg-6.col-sm-12.col-md-12
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 1. 
+            | Naciones unidas en Colombia
+          figure
+            img(src='@/assets/curso/temas/t1/fig1.png', alt='Texto alternativo: logos de la Organización panamericana de la salud (OPS) y la Organización mundial de la salud (OMS), la imagen incluye un mapa de américa y una vara de asclepio, con textos en azul que identifican a ambas entidades y su función como oficina regional para las américas.')
+
+      .row.align-items-center.mb-5.subt(data-aos="fade-right")
+        .col-auto.ico
+          img(src='@/assets/curso/temas/subt.svg', alt='')
+        .col
+          h3.mb-0.mx-3 Políticas públicas saludables
+
+      .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5(data-aos="fade-down")
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require('@/assets/curso/temas/t1/img5.png')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 La organización mundial de la salud (OMS) define las políticas públicas saludables como aquellas que integran la salud y la equidad en todas las áreas de gobierno, asegurando que las decisiones políticas favorezcan el bienestar de la población, estas políticas implican una responsabilidad compartida entre el Estado, la sociedad civil y el sector privado, priorizando la prevención de enfermedades, la promoción de hábitos saludables y la mejora del acceso a servicios esenciales.
+          br
+          br
+          | El objetivo principal es crear entornos que faciliten estilos de vida saludables, fomentando la participación social, la educación en salud y la sostenibilidad ambiental, en este contexto, la estrategia de entornos saludables (EES) se convierte en un eje fundamental para la construcción de comunidades resilientes, promoviendo la articulación intersectorial y la implementación de políticas públicas basadas en la equidad y el bienestar integral. A continuación, se presenta el mapa conceptual de los actores estratégicos de entornos saludables (EES). 
+
+      .row.justify-content-center.mt-5(data-aos="fade-left") 
+        .col-lg-10.col-sm-12.col-md-12
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 2. 
+            | Actores estratégicos de EES
+          figure
+            img(src='@/assets/curso/temas/t1/fig2.png', alt='Texto alternativo: Mapa conceptual de los actores de la Estrategia de entornos saludables (EES), que muestra la relación entre entidades como ministerios, instituciones educativas y organizaciones sociales en la promoción de entornos saludables.')
+
+
 
   </template>
 
